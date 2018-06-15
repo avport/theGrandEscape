cb40945013f1670865a90f0d794cfacf108f4393
Carly Moses <cmoses916@gmail.com>
2018-06-15 13:58:54 -0400

    added questions and almost answers to the page

--- app/public/js/components/mainRoom.js
+++ app/public/js/components/mainRoom.js
@@ -3,8 +3,13 @@
 const mainRoom = {
 
   template: `
-  <section ng-repeat="question in $ctrl.questions.$$state.value.data.results">
+  <section ng-repeat="question in $ctrl.questions">
     <h3>{{question.question}}</h3>
+    <p><input type="radio" name="answer" value="A">A {{question.["0"].answers["0"]}</p>
+    <p><input type="radio" name="answer" value="A">B {{question.answers["1"]}</p>
+    <p><input type="radio" name="answer" value="A">C {{question.answers["2"]}</p>
+    <p><input type="radio" name="answer" value="A">D {{question.answers["3"]}</p>
+
   </section>
     <h1>You're doing a great job! Keep it up!</h1>
   
@@ -14,13 +19,12 @@ const mainRoom = {
     const vm = this;
 
    TriviaService.getQuestions().then((response) => {
-     console.log(response); 
+     console.log(response);
+     vm.questions = response 
+     return response
    });
   
   }]
-
-
-
 
 }
 
