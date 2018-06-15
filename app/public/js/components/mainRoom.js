347b462eb119fe871ae2db11429171e8a9667231
Carly Moses <cmoses916@gmail.com>
2018-06-15 14:08:44 -0400

    syntax error corrected

--- app/public/js/components/mainRoom.js
+++ app/public/js/components/mainRoom.js
@@ -1,15 +1,13 @@
 "use strict";
 
 const mainRoom = {
-
   template: `
   <section ng-repeat="question in $ctrl.questions">
     <h3>{{question.question}}</h3>
-    <p><input type="radio" name="answer" value="A">A {{question.["0"].answers["0"]}</p>
-    <p><input type="radio" name="answer" value="A">B {{question.answers["1"]}</p>
-    <p><input type="radio" name="answer" value="A">C {{question.answers["2"]}</p>
-    <p><input type="radio" name="answer" value="A">D {{question.answers["3"]}</p>
-
+    <p><input type="radio" name="answer" value="A">A {{question.answers["0"]}}</p>
+    <p><input type="radio" name="answer" value="A">B {{question.answers["1"]}}</p>
+    <p><input type="radio" name="answer" value="A">C {{question.answers["2"]}}</p>
+    <p><input type="radio" name="answer" value="A">D {{question.answers["3"]}}</p>
   </section>
     <h1>You're doing a great job! Keep it up!</h1>
   
